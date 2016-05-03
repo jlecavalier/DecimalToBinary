7a26ae6113c3900e122af12b3aac340a87155995
Jay LeCavalier <jay.lecavalier@colorado.edu>
2016-05-03 00:03:37 -0600

    finally fixed the fonts on spinner items. What a pain.

--- src/com/jaylecavalier/decimaltobinary/MainActivity.java
+++ src/com/jaylecavalier/decimaltobinary/MainActivity.java
@@ -2,19 +2,27 @@ package com.jaylecavalier.decimaltobinary;
 
 // Android stuff
 import android.app.ActivityManager;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 // ActionBarSherlock stuff
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+// Java Standard Libray stuff
+import java.util.Arrays;
+import java.util.List;
 
 // My stuff
 import com.jaylecavalier.decimaltobinary.R;
@@ -25,8 +33,55 @@ public class MainActivity extends SherlockFragmentActivity {
 		super.onCreate(savedInstanceState);
 		setupActionBar();
 		setContentView(R.layout.main);
+		setupSpinners();
 		setFonts();
 		setButtonColors();
+	}
+
+    // Basically, the only reason we need to create this custom
+    // spinner adapter class is so that we can set a custom font for the spinner items.
+    // Why the developers from the android team decided to make this such a difficult thing
+    // to do? I shall never know.
+	private class MySpinnerAdapter extends ArrayAdapter<CharSequence> {
+		// Get a handle to the font we want
+	    Typeface monoFont = Typeface.createFromAsset(getApplicationContext().getAssets(), "fonts/FreeMono.ttf");
+
+	    // Constructor simply calls the real constructor.
+	    private MySpinnerAdapter(Context context, int resource, List<CharSequence> items) {
+	        super(context, resource, items);
+	    }
+
+	    // Affects default (closed) state of the spinner
+	    @Override
+	    public View getView(int position, View convertView, ViewGroup parent) {
+	        TextView tv = (TextView) super.getView(position, convertView, parent);
+	        tv.setTypeface(monoFont);
+	        tv.setTextColor(getApplicationContext().getResources().getColor(R.color.homebrew_green));
+	        return tv;
+	    }
+
+	    // Affects opened state of the spinner
+	    @Override
+	    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+	        TextView tv = (TextView) super.getDropDownView(position, convertView, parent);
+	        tv.setTypeface(monoFont);
+	        tv.setTextColor(getApplicationContext().getResources().getColor(R.color.homebrew_green));
+	        return tv;
+	    }
+	}
+
+	private void setupSpinners() {
+		Spinner spinner = (Spinner) findViewById(R.id.number_systems_spinner_1);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		MySpinnerAdapter adapter = new MySpinnerAdapter(
+        	getApplicationContext(),
+        	R.layout.spinner_item,
+        	Arrays.asList((CharSequence[]) getResources().getStringArray(R.array.number_systems_spinner))
+		);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(R.layout.spinner_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(adapter);
 	}
 
 	public void convert(View view) {
